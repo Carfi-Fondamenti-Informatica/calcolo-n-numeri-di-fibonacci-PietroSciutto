42f78ae9ecc77f5d654bd4e4050ffc3a42e8fabb
PietroSciutto <91790426+PietroSciutto@users.noreply.github.com>
2021-11-09 21:23:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+int i=0, n=0, primo=1, secondo=1, terzo=0;
+cout<<"Quanti numeri vuoi visualizzare?"<<endl;
+cin>>n;
+
+if(n>=2){
+    cout << primo << endl;
+    cout << secondo << endl;
+}else{
+    cout<<"errore"<<endl;
+}
+
+for(i=1;i<n;i++){
+    terzo=primo+secondo;
+    primo=secondo;
+    secondo=terzo;
+    cout<<terzo<<endl;
+}
    return 0;
 }
